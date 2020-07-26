47fcafba915e9c454f9f3f25fdaf1e8af3dca063
Fahad Qayyum <fahadq@my.yorku.ca>
2020-07-26 18:16:32 -0400

    Forms converted to angular material

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,39 @@
+import {NgModule} from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatDividerModule} from "@angular/material/divider";
+import {HttpClientModule} from "@angular/common/http";
+import {MatIconModule} from "@angular/material/icon";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports: [
+    FormsModule,
+    MatIconModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+  ]
+})
+export class SharedModule {
+}
